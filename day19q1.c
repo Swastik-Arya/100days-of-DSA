09803538ba7a6024cd16f171e1bbdadb543ecae3
Tushveen Kaur Bhatia <swastikarya947@gmail.com>
2026-03-25 19:21:52 +0530

    Add program to find minimum absolute sum of pairs
    Implement a program to find two numbers with the minimum absolute sum.

---
+++ day19q1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int a[n];
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    int minSum = abs(a[0] + a[1]);
+    int x = a[0], y = a[1];
+
+    for(int i = 0; i < n; i++) {
+        for(int j = i + 1; j < n; j++) {
+            int sum = abs(a[i] + a[j]);
+            if(sum < minSum) {
+                minSum = sum;
+                x = a[i];
+                y = a[j];
+            }
+        }
+    }
+
+    printf("%d %d", x, y);
+
+    return 0;
+}
