a298f8dd08ab843abbb6b76f1a4e76e6f3cbeb68
Tushveen Kaur Bhatia <swastikarya947@gmail.com>
2026-03-28 11:07:04 +0530

    Create day26.c

---
+++ day26.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *prev;
+    struct node *next;
+};
+
+int main()
+{
+    struct node *head = NULL, *temp, *newnode;
+    int n, i;
+
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++)
+    {
+        newnode = (struct node*)malloc(sizeof(struct node));
+        scanf("%d", &newnode->data);
+        newnode->prev = NULL;
+        newnode->next = NULL;
+
+        if(head == NULL)
+        {
+            head = temp = newnode;
+        }
+        else
+        {
+            temp->next = newnode;
+            newnode->prev = temp;
+            temp = newnode;
+        }
+    }
+
+    temp = head;
+
+    while(temp != NULL)
+    {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+    return 0;
+}
