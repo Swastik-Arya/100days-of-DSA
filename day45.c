182ac691cc6285e5d28af189e193862004564491
Tushveen Kaur Bhatia <swastikarya947@gmail.com>
2026-03-28 11:56:47 +0530

    Create day45.c

---
+++ day45.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node* createNode(int data)
+{
+    struct node* newnode = (struct node*)malloc(sizeof(struct node));
+    newnode->data = data;
+    newnode->left = newnode->right = NULL;
+    return newnode;
+}
+
+int height(struct node* root)
+{
+    if(root == NULL)
+        return 0;
+
+    int left = height(root->left);
+    int right = height(root->right);
+
+    if(left > right)
+        return left + 1;
+    else
+        return right + 1;
+}
+
+int main()
+{
+    int n, i;
+    scanf("%d", &n);
+
+    int arr[100];
+
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    struct node* nodes[100];
+
+    for(i = 0; i < n; i++)
+    {
+        if(arr[i] == -1)
+            nodes[i] = NULL;
+        else
+            nodes[i] = createNode(arr[i]);
+    }
+
+    for(i = 0; i < n; i++)
+    {
+        if(nodes[i] != NULL)
+        {
+            int left = 2*i + 1;
+            int right = 2*i + 2;
+
+            if(left < n)
+                nodes[i]->left = nodes[left];
+
+            if(right < n)
+                nodes[i]->right = nodes[right];
+        }
+    }
+
+    printf("%d", height(nodes[0]));
+
+    return 0;
+}
