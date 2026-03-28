d115e5376a05a5eb94cb7bbbe070466285617564
Tushveen Kaur Bhatia <swastikarya947@gmail.com>
2026-03-28 12:07:07 +0530

    Add binary search tree implementation in day49.c
    Implement a binary search tree with insertion and inorder traversal.

---
+++ day49.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node* createNode(int data)
+{
+    struct node* newnode = (struct node*)malloc(sizeof(struct node));
+    newnode->data = data;
+    newnode->left = newnode->right = NULL;
+    return newnode;
+}
+
+struct node* insert(struct node* root, int data)
+{
+    if(root == NULL)
+        return createNode(data);
+
+    if(data < root->data)
+        root->left = insert(root->left, data);
+    else
+        root->right = insert(root->right, data);
+
+    return root;
+}
+
+void inorder(struct node* root)
+{
+    if(root == NULL)
+        return;
+
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+
+int main()
+{
+    struct node* root = NULL;
+    int n, i, x;
+
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &x);
+        root = insert(root, x);
+    }
+
+    inorder(root);
+
+    return 0;
+}
