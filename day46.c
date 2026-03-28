0536c6415340d5234cd2bfa750c01095e0ee0617
Tushveen Kaur Bhatia <swastikarya947@gmail.com>
2026-03-28 11:58:56 +0530

    Create day46.c

---
+++ day46.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node* createNode(int data)
+{
+    struct node* newnode = (struct node*)malloc(sizeof(struct node));
+    newnode->data = data;
+    newnode->left = newnode->right = NULL;
+    return newnode;
+}
+
+void levelOrder(struct node* root)
+{
+    if(root == NULL)
+        return;
+
+    struct node* queue[100];
+    int front = 0, rear = 0;
+
+    queue[rear++] = root;
+
+    while(front < rear)
+    {
+        struct node* temp = queue[front++];
+        printf("%d ", temp->data);
+
+        if(temp->left != NULL)
+            queue[rear++] = temp->left;
+
+        if(temp->right != NULL)
+            queue[rear++] = temp->right;
+    }
+}
+
+int main()
+{
+    int n, i;
+    scanf("%d", &n);
+
+    int arr[100];
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    struct node* nodes[100];
+
+    for(i = 0; i < n; i++)
+    {
+        if(arr[i] == -1)
+            nodes[i] = NULL;
+        else
+            nodes[i] = createNode(arr[i]);
+    }
+
+    for(i = 0; i < n; i++)
+    {
+        if(nodes[i] != NULL)
+        {
+            int left = 2*i + 1;
+            int right = 2*i + 2;
+
+            if(left < n)
+                nodes[i]->left = nodes[left];
+
+            if(right < n)
+                nodes[i]->right = nodes[right];
+        }
+    }
+
+    levelOrder(nodes[0]);
+
+    return 0;
+}
