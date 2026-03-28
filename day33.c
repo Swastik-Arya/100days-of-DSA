7e59a91ff8d720750e479b1de61da8914532c4c1
Tushveen Kaur Bhatia <swastikarya947@gmail.com>
2026-03-28 11:24:06 +0530

    Create day33.c

---
+++ day33.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <ctype.h>
+
+char stack[100];
+int top = -1;
+
+void push(char x)
+{
+    stack[++top] = x;
+}
+
+char pop()
+{
+    if(top == -1)
+        return -1;
+    return stack[top--];
+}
+
+int priority(char x)
+{
+    if(x == '+' || x == '-')
+        return 1;
+    if(x == '*' || x == '/')
+        return 2;
+    if(x == '^')
+        return 3;
+    return 0;
+}
+
+int main()
+{
+    char infix[100], postfix[100];
+    int i = 0, k = 0;
+    
+    scanf("%s", infix);
+
+    while(infix[i] != '\0')
+    {
+        if(isalnum(infix[i]))
+        {
+            postfix[k++] = infix[i];
+        }
+        else if(infix[i] == '(')
+        {
+            push(infix[i]);
+        }
+        else if(infix[i] == ')')
+        {
+            while(stack[top] != '(')
+                postfix[k++] = pop();
+            pop();
+        }
+        else
+        {
+            while(top != -1 && priority(stack[top]) >= priority(infix[i]))
+                postfix[k++] = pop();
+            push(infix[i]);
+        }
+        i++;
+    }
+
+    while(top != -1)
+        postfix[k++] = pop();
+
+    postfix[k] = '\0';
+
+    printf("%s", postfix);
+
+    return 0;
+}
