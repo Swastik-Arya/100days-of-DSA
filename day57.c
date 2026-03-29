4b13b7b7234723a2192cf1e51cb9a1d2004db272
Tushveen Kaur Bhatia <swastikarya947@gmail.com>
2026-03-29 20:44:46 +0530

    Create day57.c

---
+++ day57.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node* createNode(int data)
+{
+    struct node* newnode = (struct node*)malloc(sizeof(struct node));
+    newnode->data = data;
+    newnode->left = newnode->right = NULL;
+    return newnode;
+}
+
+void mirror(struct node* root)
+{
+    if(root == NULL)
+        return;
+
+    struct node* temp = root->left;
+    root->left = root->right;
+    root->right = temp;
+
+    mirror(root->left);
+    mirror(root->right);
+}
+
+void inorder(struct node* root)
+{
+    if(root == NULL)
+        return;
+
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int arr[100];
+
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    struct node* nodes[100];
+
+    for(int i = 0; i < n; i++)
+    {
+        if(arr[i] == -1)
+            nodes[i] = NULL;
+        else
+            nodes[i] = createNode(arr[i]);
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+        if(nodes[i] != NULL)
+        {
+            int left = 2*i + 1;
+            int right = 2*i + 2;
+
+            if(left < n)
+                nodes[i]->left = nodes[left];
+
+            if(right < n)
+                nodes[i]->right = nodes[right];
+        }
+    }
+
+    mirror(nodes[0]);
+    inorder(nodes[0]);
+
+    return 0;
+}
